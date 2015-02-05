ca01655e5bc43a9f7b240dff5221bb67c73eda72
Michael Fraenkel <fraenkel@us.ibm.com>
2015-02-04 21:58:36 -0700

    Combine private_domains and shared_private_domains
    Phase 2
    - Merge shared_private_domains into private_domains
    - Specialized query since UNION prevents the tinyint -> bool conversion
    - Optimize domain user_visibility_filter
    
    vcap_relations prevents an exception when adding the owning organization to
    a private domain as a shared organization.
    
    [#81780716]
    
    Signed-off-by: Ashvin Agrawal <ashvin@pivotal.io>

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'digest/sha1'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = '291612731e25b8d212fccf04652c0a2f045abd1a'
+  API_FOLDER_CHECKSUM = '1c44f638cbceb17c4085066ce31cf86cecfddb83'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.22.0')
